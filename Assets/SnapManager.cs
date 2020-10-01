0d0fea8c14b8d7ad69be2c2267a9ece702e1b577
Gabriel Vazquez <gvaz@knights.ucf.edu>
2020-09-30 23:21:53 -0400

    Made it so scarecrow could be dismembered

---
+++ Assets/SnapManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapManager : MonoBehaviour
+{
+    public GameObject Scarecrow;
+    public bool SawSnapped = false;
+    public bool HammerSnapped = false;
+
+void Start()
+{
+    Scarecrow.SetActive(true);
+}
+
+public void HammerUnSnappedAction()
+    {
+        HammerSnapped = false; 
+        Debug.Log("Snapped");
+    }
+
+    public void HammerSnappedAction()
+    {
+        HammerSnapped = true;
+        Scarecrow.SetActive(false);
+        Debug.Log("Snapped");
+    }
+    public void UnSnapped()
+    {
+        SawSnapped = false; 
+        Debug.Log("Snapped");
+    }
+
+    public void Snapped()
+    {
+        SawSnapped = true;
+        Scarecrow.SetActive(false);
+        Debug.Log("Snapped");
+    }
+}
